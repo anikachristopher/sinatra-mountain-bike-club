ba71934e1161a22a36950ddf298add82e6b22521
anikachristopher <anika.mchristoper@gmail.com>
2020-02-15 11:26:24 -0500

    update migrations by creating tables

--- db/migrate/20200215161516_create_events.rb
+++ db/migrate/20200215161516_create_events.rb
@@ -1,4 +1,11 @@
 class CreateEvents < ActiveRecord::Migration
   def change
+    create_table :events do |t|
+      t.string :name
+      t.date :event_date
+      t.string :location
+      t.time :time
+      t.string :category
+    end
   end
 end
--- db/migrate/20200215161536_create_riders.rb
+++ db/migrate/20200215161536_create_riders.rb
@@ -1,4 +1,10 @@
 class CreateRiders < ActiveRecord::Migration
   def change
+    create_table :riders do |t|
+      t.string :name
+      t.string :gender
+      t.date :birthdate
+      t.string :category
+    end
   end
 end
