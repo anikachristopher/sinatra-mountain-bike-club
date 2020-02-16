5d0056c2d03b0062eb12fc493d323e98093c3da3
anikachristopher <anika.mchristoper@gmail.com>
2020-02-16 17:49:12 -0500

    crrtae post route in the users folder

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,3 +1,7 @@
 class UsersController < ApplicationController
 
+    get '/signup' do
+        erb :'/users/signup' #erb always shows you a view. thisbis the signup view located in the users folder
+  end
+
 end
