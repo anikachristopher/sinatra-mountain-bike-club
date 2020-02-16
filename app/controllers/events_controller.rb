ccacbe3a2f880a2896c25143d2781b6aa5b2d26c
anikachristopher <anika.mchristoper@gmail.com>
2020-02-16 18:46:57 -0500

    create new view in the events folder

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -3,4 +3,13 @@ class EventsController < ApplicationController
         @events = Event.all
         erb :'index'
       end
+
+      get '/events/new' do
+        erb :'new'
+      end
+       
+      post '/events' do
+        @event = Event.create(:name => params[:name], :event_date => params[:event_date], :location => params[:location], :time => params[:time], :category => params[:category])
+        redirect to "/events/#{@event.id}"
+      end
 end
